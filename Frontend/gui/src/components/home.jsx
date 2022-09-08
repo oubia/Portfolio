c0ed3bdfcf1a3704d3486ac289820a0f18ace15c
oubia <dev.oubia@gmail.com>
2022-09-08 20:20:34 +0100

    end of home design

--- Frontend/gui/src/components/home.jsx
+++ Frontend/gui/src/components/home.jsx
@@ -1,25 +1,64 @@
+import { useState,useEffect } from 'react';
 import React from 'react';
 import {Container, Row,Col} from 'react-bootstrap'
 import '../css/home.css';
 import {ArrowRightCircle} from 'react-bootstrap-icons';
+import headerImg from "../assets/headrSvg.svg"
+
 
 function Home() {
+
+  const [loopNum,setLoopNum] = useState(0);
+  const [isDeleting,setIsDeleting] = useState(false);
+  const toRotate = ['Web developer','Web designer','UI/UX Designer'];
+  const [text,setText] = useState('');
+  const [delta,setDelta] = useState(30 - Math.random()*100);
+  const period = 2000;
+
+  useEffect(()=>{
+    let ticker = setInterval(() =>{
+      tick();
+    },delta)
+    return () => { clearInterval(ticker)};
+  },[text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0,text.length - 1): fullText.substring(0,text.length + 1);
+    
+    setText(updatedText);
+    
+    if(isDeleting){
+      setDelta(prevDelta => prevDelta/2)
+    }
+    if(!isDeleting && updatedText === fullText){
+      setIsDeleting(true);
+      setDelta(period);
+    }else if(isDeleting && updatedText === ''){
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(100);
+    }
+  }
+
     return (
       <section className='home' id='home'>
-        <Container>
-          <Row className='align-items-center'>
-            <Col xs={12} md={6} xl={7}>
+          <Row className='row'>
+
+            <Col className='home-description'>
               <span className='tagline'>Welcome to my Portfolio</span>
-              <h1>{`Hi I'm Mohammed Oubia`}<span className='warp'>Data scientist software</span></h1>
+              <h1>{`Hi I'm Mohammed Oubia`}<span className='warp'> {text}</span></h1>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sincethe 1500s, when an unknown printer took a galley</p>
-              <button onClick={()=>console.log('connect selected')}>Let's connect
-              <ArrowRightCircle size={25}></ArrowRightCircle></button>
+              <button onClick={()=>console.log('connect selected')} className='connectBtn'>Let's connect<span> </span>
+              </button>
             </Col>
-            <Col>
-              {/* add the picture and complete the home page */}
+
+            <Col className='home-image'>
+              <img src={headerImg} alt="Header Img"/>
             </Col>
+            
           </Row>
-        </Container>
       </section>
     );
   }
