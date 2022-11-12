08e5aa8dda92c1f7623590b1d1d2b16c0c35420d
oubia <dev.oubia@gmail.com>
2022-11-12 22:32:55 +0100

    make it better

--- dportfolio/settings.py
+++ dportfolio/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'build/static/')
