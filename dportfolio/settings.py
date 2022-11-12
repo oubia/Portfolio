ff7ce67653a71f1d8a304d2a259eb3c22cbd3487
oubia <dev.oubia@gmail.com>
2022-11-12 22:26:15 +0100

    make it better

--- dportfolio/settings.py
+++ dportfolio/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'dportfolio.urls'
